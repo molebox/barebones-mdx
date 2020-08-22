470f7161f0f66164922f28b717bb451bb9882511
HAINES-DESKTOP\richa <richardkhaines@hotmail.co.uk>
2020-08-22 19:09:35 +0200

    redo

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {render} from 'react-dom'
+import {MDXProvider} from '@mdx-js/react'
+
+import Content from './content.mdx'
+
+const components = {
+  h1: props => <h1 style={{color: 'tomato'}} {...props} />
+}
+
+render(
+  <MDXProvider components={components}>
+    <Content />
+  </MDXProvider>,
+  document.querySelector('#demo')
+)

0b2b6a0243bc9c14955eb7c19a9f12f0141345f1
HAINES-DESKTOP\richa <richardkhaines@hotmail.co.uk>
2020-08-27 20:25:05 +0200

    updated to chakra v1

--- src/main.js
+++ src/main.js
@@ -8,43 +8,75 @@ import {
   Text,
   Heading,
   Grid,
-  ThemeProvider
+  ChakraProvider,
+  CSSReset,
 } from "@chakra-ui/core";
 import theme from "./theme";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+// import theme from "@chakra-ui/theme"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Content from "./pages/index.mdx";
 import PageTwo from "./pages/page-two.mdx";
-import Nav from './components/nav';
+import Nav from "./components/nav";
 
 const components = {
-  h1: (props) => <Text fontSize="6xl">{props.children}</Text>,
-  h2: (props) => <Text fontSize="5xl">{props.children}</Text>,
-  h3: (props) => <Text fontSize="4xl">{props.children}</Text>,
-  h4: (props) => <Text fontSize="3xl">{props.children}</Text>,
-  h5: (props) => <Text fontSize="2xl">{props.children}</Text>,
-  h6: (props) => <Text fontSize="xl">{props.children}</Text>,
+  h1: (props) => (
+    <Box as="h1" fontSize="6xl">
+      {props.children}
+    </Box>
+  ),
+  h2: (props) => (
+    <Text as="h2" fontSize="5xl">
+      {props.children}
+    </Text>
+  ),
+  h3: (props) => (
+    <Text as="h3" fontSize="4xl">
+      {props.children}
+    </Text>
+  ),
+  h4: (props) => (
+    <Text as="h4" fontSize="3xl">
+      {props.children}
+    </Text>
+  ),
+  h5: (props) => (
+    <Text as="h5" fontSize="2xl">
+      {props.children}
+    </Text>
+  ),
+  h6: (props) => (
+    <Text as="h6" fontSize="xl">
+      {props.children}
+    </Text>
+  ),
   p: (props) => <Text>{props.children}</Text>,
   Text,
   Box,
   Flex,
   Heading,
+  a: (props) => (
+    <Text as="h6" fontSize="xl">
+      {props.children}
+    </Text>
+  ),
   Grid: (props) => <Grid {...props}>{props.children}</Grid>,
 };
 
 render(
   <MDXProvider components={components}>
-    <ThemeProvider theme={theme}>
-      <MdxEmbedProvider>
-        <Router>
-        <Nav/>
+    <MdxEmbedProvider>
+      <Router>
+        <ChakraProvider theme={theme}>
+          <CSSReset />
+          <Nav />
           <Routes>
             <Route path="/" element={<Content />} />
             <Route path="/page-two" element={<PageTwo />} />
           </Routes>
-        </Router>
-      </MdxEmbedProvider>
-    </ThemeProvider>
+        </ChakraProvider>
+      </Router>
+    </MdxEmbedProvider>
   </MDXProvider>,
   document.querySelector("#root")
 );
--- src/theme.js
+++ src/theme.js
@@ -1,4 +1,4 @@
-import { theme } from "@chakra-ui/core";
+import theme from "@chakra-ui/theme";
 
 export default {
   ...theme,
@@ -9,5 +9,17 @@ export default {
       800: "#153e75",
       700: "#2a69ac",
     },
+  },
+  fontSizes: {
+    xs: "12px",
+    sm: "14px",
+    md: "16px",
+    lg: "18px",
+    xl: "20px",
+    "2xl": "24px",
+    "3xl": "28px",
+    "4xl": "36px",
+    "5xl": "48px",
+    "6xl": "64px",
   },
 };

7f543e6fef2d0bb174a501f217a5859fcc543823
HAINES-DESKTOP\richa <richardkhaines@hotmail.co.uk>
2020-09-03 08:32:12 +0200

    changed outDir to public

---
+++ bundle.js
@@ -0,0 +1,17 @@
+import Bundler from 'parcel-bundler';
+import Path from 'path';
+const __dirname = Path.resolve();
+
+
+(async function() {
+    const entryFiles = Path.join(__dirname, './index.html');
+    const options = {
+    outDir: './public'
+}
+    // Initializes a bundler using the entrypoint location and options provided
+    const bundler = new Bundler(entryFiles, options);
+  
+    // Run the bundler, this returns the main bundle
+    // Use the events if you're using watch mode as this promise will only trigger once and not for every rebuild
+    const bundle = await bundler.bundle();
+  })();
